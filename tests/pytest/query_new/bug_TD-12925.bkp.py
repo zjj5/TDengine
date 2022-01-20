09177f824709433adaf1d8cb59bf83f6aa9bd695
“happyguoxy” <happy_guoxy@163.com>
2022-01-20 11:39:54 +0800

    gxy

---
+++ tests/pytest/query_new/bug_TD-12925.bkp.py
@@ -0,0 +1,89 @@
+###################################################################
+#           Copyright (c) 2016 by TAOS Technologies, Inc.
+#                     All rights reserved.
+#
+#  This file is proprietary and confidential to TAOS Technologies.
+#  No part of this file may be reproduced, stored, transmitted,
+#  disclosed or used in any form or by any means other than as
+#  expressly provided by the written permission from Jianhui Tao
+#
+###################################################################
+
+# -*- coding: utf-8 -*-
+
+import random
+import string
+import os
+import sys
+import time
+import taos
+from util.log import tdLog
+from util.cases import tdCases
+from util.sql import tdSql
+from util.dnodes import tdDnodes
+from util.dnodes import *
+from util.createdata import *
+from util.where import *
+import itertools
+from itertools import product
+from itertools import combinations
+from faker import Faker
+import subprocess
+
+class TDTestCase:
+    def caseDescription(self):
+        '''
+        case1<xyguo>:select * from regular_table where condition && select * from ( select front )
+        ''' 
+        return
+
+    def init(self, conn, logSql):
+        tdLog.debug("start to execute %s" % __file__)
+        tdSql.init(conn.cursor(), logSql)
+
+        testcaseFilename = os.path.split(__file__)[-1]
+        os.system("rm -rf query_new/%s.sql" % testcaseFilename )
+
+    def run(self):
+        tdSql.prepare()
+
+        db = "regular_db"
+        tdCreateData.dropandcreateDB_random("%s" %db,1) 
+
+        conn1 = taos.connect(host="127.0.0.1", user="root", password="taosdata", config="/etc/taos/")
+        print(conn1)
+        cur1 = conn1.cursor()
+        tdSql.init(cur1, True)        
+        cur1.execute('use "%s";' %db)
+        sql = 'select * from regular_table_1 limit 5;'
+        cur1.execute(sql)
+
+        for i in range(2):
+            try:
+                testcaseFilename = os.path.split(__file__)[-1]
+                taos_cmd1 = "taos -f query_new/%s.sql" % testcaseFilename
+                _ = subprocess.check_output(taos_cmd1, shell=True).decode("utf-8")
+                print(conn1)
+
+                for i in range(2):
+                    print(db)
+                    cur1.execute('use "%s";' %db)                 
+
+                    print("=========================================case1=========================================")
+
+                    #sql = "select min(q_smallint_null) like max(q_smallint) from table_2 where q_smallint is null and  q_tinyint != 128 and  q_binary nmatch '123_' and  (q_bool = true or q_bool = false) session(ts,8d);"
+                    #sql = "select round(q_double_null) != round(q_double_null) from stable_2 where q_float >= -3.4e38 and q_double <= 1.7e308 and (q_bool = true or q_bool = false);"
+                    sql = "select round(q_double_null) != round(q_double_null) from table_2 "
+                    tdSql.query(sql)
+
+
+            except Exception as e:
+                raise e   
+
+    def stop(self):
+        tdSql.close()
+        tdLog.success("%s successfully executed" % __file__)
+
+
+tdCases.addWindows(__file__, TDTestCase())
+tdCases.addLinux(__file__, TDTestCase())
