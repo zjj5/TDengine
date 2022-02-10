eb347bcf0a32834d7cb8cf4205a03856748a0632
“happyguoxy” <happy_guoxy@163.com>
2022-02-10 11:49:00 +0800

    gxy

--- tests/pytest/query_new/regular_query_null.py
+++ tests/pytest/query_new/regular_query_null.py
@@ -46,162 +46,217 @@ class TDTestCase:
         ''' 
         return
 
+    #basic_param
+    db = "regular_db"
+    table_list = ['regular_table_1','stable_1_1','regular_table_2','stable_1_2','stable_2_1']
+    table = str(random.sample(table_list,1)).replace("[","").replace("]","").replace("'","")
+    table_null_list = ['regular_table_null','stable_1_3','stable_1_4','stable_2_2','stable_null_data_1']
+    table_null = str(random.sample(table_null_list,1)).replace("[","").replace("]","").replace("'","")
+    testcaseFilename = os.path.split(__file__)[-1]
+
     def init(self, conn, logSql):
         tdLog.debug("start to execute %s" % __file__)
         tdSql.init(conn.cursor(), logSql)
 
-        testcaseFilename = os.path.split(__file__)[-1]
-        os.system("rm -rf query_new/%s.sql" % testcaseFilename )
+    def case_common(self):
+        os.system("rm -rf query_new/%s.sql" % self.testcaseFilename )    
+        tdCreateData.dropandcreateDB_random("%s" %self.db,1) 
+
+        conn1 = taos.connect(host="127.0.0.1", user="root", password="taosdata", config="/etc/taos/")
+        cur1 = conn1.cursor()
+        tdSql.init(cur1, True)        
+        cur1.execute('use "%s";' %self.db)
+        sql = 'select * from regular_table_1 limit 5;'
+        cur1.execute(sql)
+
+        return(conn1,cur1)
+
+    def right_case1(self):
+        print("case1:select * from regular_table where condition[null data] && select * from ( select front )")
+        print("\n\n\n=========================================case1=========================================\n\n\n")
+        
+        case_common = self.case_common()
+        conn1 = case_common[0]
+        cur1 = case_common[1]
+
+        for i in range(2):
+            try:
+                taos_cmd1 = "taos -f query_new/%s.sql" % self.testcaseFilename
+                _ = subprocess.check_output(taos_cmd1, shell=True).decode("utf-8")
+                print(conn1)
+                cur1.execute('use "%s";' %self.db)                 
+
+                regular_where_null = tdWhere.regular_where_null()
+                sql1 = 'select * from %s;'  % self.table
+                for i in range(2,len(regular_where_null[2])+1):
+                    q_where = list(combinations(regular_where_null[2],i))
+                    for q_where in q_where:
+                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
+                        q_like_match = regular_where_null[3]
+                        q_in_where = regular_where_null[4]
+                        
+                        sql2 = "select * from %s where %s %s %s " %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s where %s %s %s )" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s) where %s %s %s " %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+            
+            except Exception as e:
+                raise e   
+
+    def right_case2(self):
+        print("case2:select * from regular_table where condition[null data] order by ts asc | desc && select * from ( select front )")
+        print("\n\n\n=========================================case2=========================================\n\n\n")
+        
+        case_common = self.case_common()
+        conn1 = case_common[0]
+        cur1 = case_common[1]
+
+        for i in range(2):
+            try:
+                taos_cmd1 = "taos -f query_new/%s.sql" % self.testcaseFilename
+                _ = subprocess.check_output(taos_cmd1, shell=True).decode("utf-8")
+                print(conn1)
+                cur1.execute('use "%s";' %self.db)                               
+
+                regular_where_null = tdWhere.regular_where_null()
+                sql1 = 'select * from %s ;' % self.table
+                for i in range(2,len(regular_where_null[2])+1):
+                    q_where = list(combinations(regular_where_null[2],i))
+                    for q_where in q_where:
+                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
+                        q_like_match = regular_where_null[3]
+                        q_in_where = regular_where_null[4]
+
+                        sql2 = "select * from %s where %s %s %s order by ts" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s where %s %s %s order by ts)" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s) where %s %s %s order by ts" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+                
+                regular_where_null = tdWhere.regular_where_null()
+                sql1 = 'select * from %s order by ts desc;' % self.table
+                for i in range(2,len(regular_where_null[2])+1):
+                    q_where = list(combinations(regular_where_null[2],i))
+                    for q_where in q_where:
+                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
+                        q_like_match = regular_where_null[3]
+                        q_in_where = regular_where_null[4]
+
+                        sql2 = "select * from %s where %s %s %s order by ts desc" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s where %s %s %s order by ts desc)" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s) where %s %s %s order by ts desc" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+            
+            except Exception as e:
+                raise e  
+
+    def right_case3(self):
+        print("case3:select * from regular_table where condition[null data] order by ts limit && select * from ( select front )")
+        print("\n\n\n=========================================case3=========================================\n\n\n")
+        
+        case_common = self.case_common()
+        conn1 = case_common[0]
+        cur1 = case_common[1]
+
+        for i in range(2):
+            try:
+                taos_cmd1 = "taos -f query_new/%s.sql" % self.testcaseFilename
+                _ = subprocess.check_output(taos_cmd1, shell=True).decode("utf-8")
+                print(conn1)
+                cur1.execute('use "%s";' %self.db)                 
+
+                regular_where_null = tdWhere.regular_where_null()
+                sql1 = 'select * from %s;' % self.table
+                for i in range(2,len(regular_where_null[2])+1):
+                    q_where = list(combinations(regular_where_null[2],i))
+                    for q_where in q_where:
+                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
+                        q_like_match = regular_where_null[3]
+                        q_in_where = regular_where_null[4]
+
+                        sql2 = "select * from %s where %s %s %s order by ts limit 10" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s where %s %s %s order by ts limit 10)" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s) where %s %s %s order by ts limit 10" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+            
+            except Exception as e:
+                raise e  
+
+    def right_case4(self):
+        print("case4:select * from regular_table where condition[null data] order by ts limit offset && select * from ( select front )")
+        print("\n\n\n=========================================case4=========================================\n\n\n")
+        
+        case_common = self.case_common()
+        conn1 = case_common[0]
+        cur1 = case_common[1]
+
+        for i in range(2):
+            try:
+                taos_cmd1 = "taos -f query_new/%s.sql" % self.testcaseFilename
+                _ = subprocess.check_output(taos_cmd1, shell=True).decode("utf-8")
+                print(conn1)
+                cur1.execute('use "%s";' %self.db)                 
+
+                regular_where_null = tdWhere.regular_where_null()
+                sql1 = 'select * from %s limit 10 offset 5;' % self.table
+                for i in range(2,len(regular_where_null[2])+1):
+                    q_where = list(combinations(regular_where_null[2],i))
+                    for q_where in q_where:
+                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
+                        q_like_match = regular_where_null[3]
+                        q_in_where = regular_where_null[4]
+
+                        sql2 = "select * from %s where %s %s %s order by ts limit 10 offset 5" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s where %s %s %s order by ts limit 10 offset 5)" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s) where %s %s %s order by ts limit 10 offset 5" %(self.table,q_where,q_like_match,q_in_where)
+                        tdCreateData.result_0(sql2)
+                        cur1.execute(sql2)
+            
+            except Exception as e:
+                raise e  
+
 
     def run(self):
         tdSql.prepare()
         startTime = time.time() 
 
-        db = "regular_db_null"
-        tdCreateData.dropandcreateDB_random("%s" %db,1) 
-
-        table_list = ['regular_table_1','stable_1_1','regular_table_2','stable_1_2','stable_2_1']
-        table = str(random.sample(table_list,1)).replace("[","").replace("]","").replace("'","")
-        table_null_list = ['regular_table_null','stable_1_3','stable_1_4','stable_2_2','stable_null_data_1']
-        table_null = str(random.sample(table_null_list,1)).replace("[","").replace("]","").replace("'","")
-
-        conn1 = taos.connect(host="127.0.0.1", user="root", password="taosdata", config="/etc/taos/")
-        print(conn1)
-        cur1 = conn1.cursor()
-        tdSql.init(cur1, True)        
-        cur1.execute('use "%s";' %db)
-        sql = 'select * from regular_table_1 limit 5;'
-        cur1.execute(sql)      
-        print(conn1)
-
-        for i in range(2):
-            try:
-                testcaseFilename = os.path.split(__file__)[-1]
-                taos_cmd1 = "taos -f query_new/%s.sql" % testcaseFilename
-                _ = subprocess.check_output(taos_cmd1, shell=True).decode("utf-8")
-                print(conn1)
-                cur1.execute('use "%s";' %db)                 
-
-                print("case1:select * from regular_table where condition[null data] && select * from ( select front )")
-                print("\n\n\n=========================================case1=========================================\n\n\n")
-
-                regular_where_null = tdWhere.regular_where_null()
-                sql1 = 'select * from %s;'  % table
-                for i in range(2,len(regular_where_null[2])+1):
-                    q_where = list(combinations(regular_where_null[2],i))
-                    for q_where in q_where:
-                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
-                        q_like_match = regular_where_null[3]
-                        q_in_where = regular_where_null[4]
-                        
-                        sql2 = "select * from %s where %s %s %s " %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s where %s %s %s )" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s) where %s %s %s " %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                print("case2:select * from regular_table where condition[null data] order by ts asc | desc && select * from ( select front )")
-                print("\n\n\n=========================================case2=========================================\n\n\n")
-
-                regular_where_null = tdWhere.regular_where_null()
-                sql1 = 'select * from %s ;' % table
-                for i in range(2,len(regular_where_null[2])+1):
-                    q_where = list(combinations(regular_where_null[2],i))
-                    for q_where in q_where:
-                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
-                        q_like_match = regular_where_null[3]
-                        q_in_where = regular_where_null[4]
-
-                        sql2 = "select * from %s where %s %s %s order by ts" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s where %s %s %s order by ts)" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s) where %s %s %s order by ts" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-                
-                regular_where_null = tdWhere.regular_where_null()
-                sql1 = 'select * from %s order by ts desc;' % table
-                for i in range(2,len(regular_where_null[2])+1):
-                    q_where = list(combinations(regular_where_null[2],i))
-                    for q_where in q_where:
-                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
-                        q_like_match = regular_where_null[3]
-                        q_in_where = regular_where_null[4]
-
-                        sql2 = "select * from %s where %s %s %s order by ts desc" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s where %s %s %s order by ts desc)" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s) where %s %s %s order by ts desc" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                print("case3:select * from regular_table where condition[null data] order by ts limit && select * from ( select front )")
-                print("\n\n\n=========================================case3=========================================\n\n\n")
-
-                regular_where_null = tdWhere.regular_where_null()
-                sql1 = 'select * from %s;' % table
-                for i in range(2,len(regular_where_null[2])+1):
-                    q_where = list(combinations(regular_where_null[2],i))
-                    for q_where in q_where:
-                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
-                        q_like_match = regular_where_null[3]
-                        q_in_where = regular_where_null[4]
-
-                        sql2 = "select * from %s where %s %s %s order by ts limit 10" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s where %s %s %s order by ts limit 10)" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s) where %s %s %s order by ts limit 10" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                print("case4:select * from regular_table where condition[null data] order by ts limit offset && select * from ( select front )")
-                print("\n\n\n=========================================case4=========================================\n\n\n")
-
-                regular_where_null = tdWhere.regular_where_null()
-                sql1 = 'select * from %s limit 10 offset 5;' % table
-                for i in range(2,len(regular_where_null[2])+1):
-                    q_where = list(combinations(regular_where_null[2],i))
-                    for q_where in q_where:
-                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
-                        q_like_match = regular_where_null[3]
-                        q_in_where = regular_where_null[4]
-
-                        sql2 = "select * from %s where %s %s %s order by ts limit 10 offset 5" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s where %s %s %s order by ts limit 10 offset 5)" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-
-                        sql2 = "select * from (select * from %s) where %s %s %s order by ts limit 10 offset 5" %(table,q_where,q_like_match,q_in_where)
-                        tdCreateData.result_0(sql2)
-                        cur1.execute(sql2)
-                        
-
-            except Exception as e:
-                raise e   
+        self.right_case1()
+        self.right_case2()
+        self.right_case3()
+        self.right_case4()
 
         endTime = time.time()
         print("total time %ds" % (endTime - startTime))
