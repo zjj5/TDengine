84278678a3d1cbe0e3c47273b53fc5080424beef
“happyguoxy” <happy_guoxy@163.com>
2022-02-14 09:28:56 +0800

    gxy

--- tests/pytest/query_new/regular_ls.py
+++ tests/pytest/query_new/regular_ls.py
@@ -24,6 +24,7 @@ from util.dnodes import tdDnodes
 from util.dnodes import *
 from util.createdata import *
 from util.where_makesql import *
+from util.where import *
 import itertools
 from itertools import product
 from itertools import combinations
@@ -51,7 +52,7 @@ class TDTestCase:
         tdCreateData.dropandcreateDB_random("%s" %db,1) 
 
         table_list = ['regular_table_1','table_1','regular_table_2','table_2']
-        table_list = ['stable_1','stable_2']
+        #table_list = ['stable_1','stable_2']
         table = str(random.sample(table_list,1)).replace("[","").replace("]","")
         table_null_list = ['table_null','regular_table_null']
         table_null = str(random.sample(table_null_list,1)).replace("[","").replace("]","")
@@ -62,27 +63,42 @@ class TDTestCase:
         tdSql.init(cur1, True)        
         cur1.execute('use "%s";' %db)
         sql = 'select * from regular_table_1 limit 5;'
-        cur1.execute(sql)
-        for data in cur1:
-            print("ts = %s" %data[0])       
+        cur1.execute(sql)      
         print(conn1)
 
-        for i in range(1):
+        for i in range(2):
             try:
                 testcaseFilename = os.path.split(__file__)[-1]
                 taos_cmd1 = "taos -f query_new/%s.sql" % testcaseFilename
                 _ = subprocess.check_output(taos_cmd1, shell=True).decode("utf-8")
                 print(conn1)
+                cur1.execute('use "%s";' %db)                 
 
-                for i in range(1):
-                    print(db)
-                    cur1.execute('use "%s";' %db)                 
+                print("case1:select * from regular_table where condition && select * from ( select front )")
+                print("\n\n\n=========================================case1=========================================\n\n\n")
 
-                    print("case1:select * from regular_table where condition && select * from ( select front )")
-                    print("=========================================case1=========================================")
+                regular_where = tdWhere.regular_where()
+                sql1 = 'select * from %s;'  % table
+                for i in range(2,len(regular_where[2])+1):
+                    q_where = list(combinations(regular_where[2],i))
+                    for q_where in q_where:
+                        q_where = str(q_where).replace("(","").replace(")","").replace("'","").replace("\"","").replace(",","")
+                        q_in_where = regular_where[4]
+                        sql2 = "select * from %s where %s %s " %(table,q_where,q_in_where)
+                        tdCreateData.dataequal('%s' %sql1 ,10,10,'%s' %sql2 ,10,10)
+                        cur1.execute(sql2)
 
-                    regular_where = tdWhere_makesql.regular_where()
-                    tdWhere_makesql.altertable()
+                        sql2 = "select * from %s t1,table_2 t2 where t1.ts = t2.ts and %s %s " %(table,q_where,q_in_where)
+                        tdCreateData.dataequal('%s' %sql1 ,10,10,'%s' %sql2 ,10,10)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s where %s %s )" %(table,q_where,q_in_where)
+                        tdCreateData.dataequal('%s' %sql1 ,10,10,'%s' %sql2 ,10,10)
+                        cur1.execute(sql2)
+
+                        sql2 = "select * from (select * from %s) where %s %s " %(table,q_where,q_in_where)
+                        tdCreateData.dataequal('%s' %sql1 ,10,10,'%s' %sql2 ,10,10)
+                        cur1.execute(sql2)
 
 
             except Exception as e:
