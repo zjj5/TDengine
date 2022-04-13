68aeecb61ba31f5bd421ce972c31fcb9c5f89b3d
Minghao Li <castermode@gmail.com>
2022-04-13 13:27:42 +0800

    integration sync

--- source/libs/sync/test/syncWriteTest.cpp
+++ source/libs/sync/test/syncWriteTest.cpp
@@ -28,6 +28,8 @@ SSyncFSM * pFsm;
 SWal *     pWal;
 SSyncNode *gSyncNode;
 
+const char* pDir = "./syncWriteTest";
+
 void CommitCb(struct SSyncFSM *pFsm, const SRpcMsg *pMsg, SyncIndex index, bool isWeak, int32_t code,
               ESyncState state) {
   char logBuf[256];
@@ -67,7 +69,7 @@ SSyncNode *syncNodeInit() {
   syncInfo.queue = gSyncIO->pMsgQ;
   syncInfo.FpEqMsg = syncIOEqMsg;
   syncInfo.pFsm = pFsm;
-  snprintf(syncInfo.path, sizeof(syncInfo.path), "%s", "./write_test");
+  snprintf(syncInfo.path, sizeof(syncInfo.path), "%s", pDir);
 
   int code = walInit();
   assert(code == 0);
@@ -107,6 +109,8 @@ SSyncNode *syncNodeInit() {
   gSyncIO->FpOnSyncAppendEntriesReply = pSyncNode->FpOnAppendEntriesReply;
   gSyncIO->FpOnSyncTimeout = pSyncNode->FpOnTimeout;
   gSyncIO->pSyncNode = pSyncNode;
+
+  syncNodeStart(pSyncNode);
 
   return pSyncNode;
 }
